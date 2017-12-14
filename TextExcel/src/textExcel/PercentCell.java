d22a30826b8dc8643bc63b42161d22fe2000836e
s-wonget <s-wonget@51S50023594.it.bsd405.org>
2017-12-14 13:19:04 -0800

    CP Final?

--- TextExcel/src/textExcel/PercentCell.java
+++ TextExcel/src/textExcel/PercentCell.java
@@ -16,13 +16,18 @@ public class PercentCell extends RealCell{
 	}
 	
 	public String abbreviatedCellText(){
-		return Spreadsheet.fillSpaces(inputText.substring(0, inputText.indexOf(".")) + "%");
+		if(pValue >= 1)
+			return Spreadsheet.fillSpaces(inputText.substring(0, inputText.indexOf(".")) + "%");
+		else
+			return Spreadsheet.fillSpaces("0%");
 	}
 	
 	public String fullCellText(){
-		if(inputText.indexOf(".") <= 2){
-			return "0." +inputText.substring(0,inputText.indexOf(".")) + inputText.substring(inputText.indexOf(".")+1,inputText.length()-1);
-		}
-		return "lol";
+		if(inputText.indexOf(".") >= 2)
+			return "0." + inputText.substring(0,inputText.indexOf(".")) + inputText.substring(inputText.indexOf(".")+1,inputText.length()-1);
+		else if(inputText.indexOf(".") == 1)
+			return "0.0" + inputText.substring(0,1) + inputText.substring(2,inputText.length()-1);
+		else
+			return "0.00" + inputText.substring(1);
 	}
 }
