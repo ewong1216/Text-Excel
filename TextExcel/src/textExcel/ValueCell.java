44cb4923056f3369dac83856d116f502a7383d49
s-wonget <s-wonget@51S50023594.it.bsd405.org>
2017-12-14 13:02:48 -0800

    CP Final

--- TextExcel/src/textExcel/ValueCell.java
+++ TextExcel/src/textExcel/ValueCell.java
@@ -3,13 +3,21 @@ package textExcel;
 public class ValueCell extends RealCell{
 	
 	private double value;
+	private String strValue;
 	
 	public ValueCell(String input){
 		super(input);
-		value = Double.parseDouble(input);
+		if(!input.contains(".")){
+			strValue = input + ".0";
+		}
+		value = Double.parseDouble(strValue);
 	}
 	
 	public double getDoubleValue(){
 		return value;
+	}
+	
+	public String abbreviatedCellText(){
+		return Spreadsheet.fillSpaces(strValue);
 	}
 }
