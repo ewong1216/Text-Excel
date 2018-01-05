ff1cfb94d2908b8e73166cb3fb0673564160b860
s-wonget <s-wonget@51S50023594.it.bsd405.org>
2018-01-04 21:06:17 -0800

    hmmm thinking

--- TextExcel/src/textExcel/Spreadsheet.java
+++ TextExcel/src/textExcel/Spreadsheet.java
@@ -108,8 +108,9 @@ public class Spreadsheet implements Grid{
 				return "";
 		}
 		if(coms.length == 3){
-			System.out.println("get into correct spot");
-			if(coms[2].contains("+") || /*coms[2].contains("-") || */ coms[2].contains("*") || coms[2].contains("/")){
+			if(coms[2].contains(" -"))
+				return "";
+			if(coms[2].contains("+") || coms[2].contains("-") ||  coms[2].contains("*") || coms[2].contains("/")){
 				if(!coms[2].contains("(") || !coms[2].contains(")") || !coms[2].contains(" ")){
 					return "ERROR: Invalid command.\n";
 				}
