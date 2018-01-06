0a101fceec65a817b321da627438ad28ed89040d
s-wonget <s-wonget@51S50023594.it.bsd405.org>
2018-01-06 13:59:50 -0800

    File IO EC

--- TextExcel/src/textExcel/PercentCell.java
+++ TextExcel/src/textExcel/PercentCell.java
@@ -6,27 +6,8 @@ public class PercentCell extends RealCell{
 	private double doubleValue;
 	
 	public PercentCell(String input){
-		input = input.substring(0, input.length()-1);
-		int dot = input.indexOf(".");
-		percentValue = Double.parseDouble(input.substring(0,input.length()));
+		percentValue = Double.parseDouble(input.substring(0,input.length()-1));
 		doubleValue = percentValue / 100;
-		/*
-		 * 		if(!input.contains("."))
-			doubleValue = Double.parseDouble(input.substring(0, input.length()-2) + "." + input.substring(input.length()-2,input.length()));
-		else if(percentValue < 1 && percentValue >= 0)
-			doubleValue = Double.parseDouble("0.00" + input.substring(dot+1));
-		else if(percentValue >= 100 || percentValue <= -100)
-			doubleValue = Double.parseDouble(input.substring(0,dot-2) + "." + input.substring(dot+1,input.length()));
-		else if(percentValue < 100 & percentValue >= 10)
-			doubleValue = Double.parseDouble("0." + input.substring(0,dot) + input.substring(dot+1,input.length()));
-		else if(percentValue > -100 && percentValue <= -10)
-			doubleValue = Double.parseDouble("-0." + input.substring(1,dot) + input.substring(dot+1,input.length()));
-		else if(percentValue < 10 && percentValue >= 1)
-			doubleValue = Double.parseDouble("0.00" + input.charAt(0) + input.substring(dot+1));
-		else 
-			doubleValue = Double.parseDouble("-0.00" + input.charAt(1) + input.substring(dot+1));
-		 */
-
 	}
 	public PercentCell(Double dValue){
 		doubleValue = dValue;
@@ -34,10 +15,9 @@ public class PercentCell extends RealCell{
 	}
 	
 	public double getDoubleValue(){
-		System.out.println(abbreviatedCellText());
-		System.out.println(fullCellText());
-		return percentValue;
+		return doubleValue;
 	}
+	
 	public String abbreviatedCellText(){
 		if(percentValue > 0)
 			return Spreadsheet.fillSpaces((int) (Math.floor(percentValue)) + "%");
@@ -48,10 +28,4 @@ public class PercentCell extends RealCell{
 	public String fullCellText(){
 		return doubleValue + "";
 	}
-	
-	public static void main(String[] args){
-		PercentCell pc = new PercentCell("1234%");
-		pc.getDoubleValue();
-		System.out.println(1.1 / 100);
-	} 
 }
--- TextExcel/src/textExcel/Spreadsheet.java
+++ TextExcel/src/textExcel/Spreadsheet.java
@@ -1,9 +1,5 @@
 package textExcel;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Arrays;
 
 public class Spreadsheet implements Grid{
